9ad9cb8e895588cc99a37133bc0abeea7aacc980
ndhieu2701 <hieu@v7rx3.onmicrosoft.com>
2022-11-28 22:17:15 +0700

    update date time picker

--- src/components/DatePicker.js
+++ src/components/DatePicker.js
@@ -1,29 +1,41 @@
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
-import { Box, TextField } from "@mui/material";
-import {
-  LocalizationProvider,
-  StaticDateTimePicker,
-} from "@mui/x-date-pickers";
+import { Box, Stack, TextField } from "@mui/material";
+import { LocalizationProvider, DateTimePicker } from "@mui/x-date-pickers";
 import React, { useState } from "react";
 
 export default function StaticDateRangePickerDemo() {
-  const [value, setValue] = useState([null, null]);
+  const [checkin, setCheckin] = useState(null);
+  const [checkout, setCheckout] = useState(null);
 
   return (
     <LocalizationProvider dateAdapter={AdapterDateFns}>
-      <StaticDateTimePicker
-        value={value}
-        onChange={(newValue) => {
-          setValue(newValue);
-        }}
-        renderInput={(startProps, endProps) => (
-          <React.Fragment>
-            <TextField {...startProps} />
-            <Box sx={{ mx: 2, my: 2 }}> to </Box>
-            <TextField {...endProps} />
-          </React.Fragment>
-        )}
-      />
+      <Stack direction="row" spacing={3} alignItems="center">
+        <DateTimePicker
+          value={checkin}
+          inputFormat="MM/dd/yyyy hh:mm a"
+          onChange={(newValue) => {
+            setCheckin(newValue);
+          }}
+          renderInput={(params) => (
+            <React.Fragment>
+              <TextField {...params} />
+            </React.Fragment>
+          )}
+        />
+        <Box sx={{ mx: 2, my: 2 }}> to </Box>
+        <DateTimePicker
+          value={checkout}
+          inputFormat="MM/dd/yyyy hh:mm a"
+          onChange={(newValue) => {
+            setCheckout(newValue);
+          }}
+          renderInput={(params) => (
+            <React.Fragment>
+              <TextField {...params} />
+            </React.Fragment>
+          )}
+        />
+      </Stack>
     </LocalizationProvider>
   );
 }
